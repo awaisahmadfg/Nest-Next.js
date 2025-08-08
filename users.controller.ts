7d0cc254e5d90fda2e1971ed67f1a80fee496ac2
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2025-08-08 18:48:08 +0500

    Create users.controller.ts

---
+++ users.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+    @Get() // GET /users or /users?role=value (Query parameter)
+    findAll(@Query('role') role?: 'ADMIN' | 'BROKERAGE' | 'VIEWER' | 'INSPECTOR' | 'GOVOFFICIAL') {
+        return []
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Query('id') id: string) {
+        return {id}
+    }
+
+    @Post() // POST /users
+    create(@Body() user: {}) {
+        return {user}
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return {userUpdate}
+    }
+
+    @Delete(':id') // DELETE /users/:id
+    delete(@Param('id') id: string) {
+        return {id}
+    }
+}
