b679b256b538144b1e83b2323892ef12fe259b55
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2025-10-29 23:47:40 +0500

    Create sqs.module.ts

---
+++ sqs.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SqsService } from './sqs.service';
+import { BlockchainConsumer } from './consumers/blockchain.consumer';
+import { PrismaModule } from '../prisma/prisma.module';
+import { LambdaModule } from '../lambda/lambda.module';
+import { EmailModule } from '../email/email.module';
+
+@Module({
+  imports: [ConfigModule, PrismaModule, LambdaModule, EmailModule],
+  providers: [SqsService, BlockchainConsumer],
+  exports: [SqsService],
+})
+export class SqsModule {}
